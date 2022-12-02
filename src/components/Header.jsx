028099fdaab4c3ccbd2109634d42f684412b0b3e
FikretDuzceker <fikretduzceker5@gmail.com>
2022-12-02 12:32:07 +0300

    Time and validation added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,47 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 const Header = () => {
+  const [clock, setClock] = useState("");
+  const [greetings, setGreetings] = useState("");
+
+  useEffect(() => {
+    setInterval(() => {
+      setClock(new Date().toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    day: "numeric",
+    month: "long",
+  };
+  const date = new Date().toLocaleDateString("en-US", options);
+
+  useEffect(() => {
+    if (clock.slice(0, 2) > 6 && clock.slice(0, 2) <= 11) {
+      setGreetings("Good Morning");
+    } else if (clock.slice(0, 2) > 11 && clock.slice(0, 2) <= 15) {
+      setGreetings("Good Afternoon");
+    } else if (clock.slice(0, 2) > 15 && clock.slice(0, 2) <= 21) {
+      setGreetings("Good Evening");
+    } else if (clock.slice(0, 2) > 22 || clock.slice(0, 2) <= 6) {
+      setGreetings("Good Night");
+    }
+  }, [clock]);
+
   return (
-    <div
-      className="Header
-  "
-    >
-      Header
-    </div>
+    <section className="header__container">
+      <h1 className="sr-only">HELLO</h1>
+      <span className="header__date"> {date} </span>
+      {/* <div className="header__brand">
+        <h2 className="header__title">ToDo</h2>
+      </div> */}
+      <div className="header__period">
+        <h3 className="header__greetings"> {greetings}</h3>
+        <h4 className="header__clock"> {clock} </h4>
+      </div>
+    </section>
   );
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,24 +4,24 @@ import { FaTimes } from "react-icons/fa";
 
 const Main = () => {
   const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Study React Pre-Class Notes",
-      day: "Feb 5th at 2:30pm",
-      isDone: false,
-    },
-    {
-      id: 2,
-      text: "Feed the Dog",
-      day: "Feb 6th at 1:30pm",
-      isDone: false,
-    },
-    {
-      id: 3,
-      text: "Attend in-Class",
-      day: "Feb 7th at 20:00pm",
-      isDone: false,
-    },
+    // {
+    //   id: 1,
+    //   text: "Study React Pre-Class Notes",
+    //   day: "Feb 5th at 2:30pm",
+    //   isDone: false,
+    // },
+    // {
+    //   id: 2,
+    //   text: "Feed the Dog",
+    //   day: "Feb 6th at 1:30pm",
+    //   isDone: false,
+    // },
+    // {
+    //   id: 3,
+    //   text: "Attend in-Class",
+    //   day: "Feb 7th at 20:00pm",
+    //   isDone: false,
+    // },
   ]);
   const [text, setText] = useState("");
   const [time, setTime] = useState("");
@@ -69,12 +69,18 @@ const Main = () => {
       <button
         className="btn"
         onClick={() => {
-          setTasks([
-            ...tasks,
-            { id: Date.now(), text, day: time, isDone: false },
-          ]);
-          setTime("");
-          setText("");
+          if (text.length !== 0 && time.length !== 0) {
+            setTasks([
+              ...tasks,
+              { id: Date.now(), text, day: time, isDone: false },
+            ]);
+            setTime("");
+            setText("");
+          } else if (text.length !== 0) {
+            alert("Lütfen tarih giriniz");
+          } else if (time.length !== 0) {
+            alert("Lütfen iş giriniz");
+          }
         }}
       >
         Add Task
